3cd7bf0456a4cd9d5501229fc28ae74217d09fdf
DVMaja <majadreilinger@gmail.com>
2023-04-12 20:34:43 +0200

    dobozok elhelyezkedese ok

--- JS/public/publicScript.js
+++ JS/public/publicScript.js
@@ -14,14 +14,13 @@ function divekOsszerak(OBJEKTUMLISTA) {
   let text = `<div class="tarolo row">`;
 
   for (let index = 0; index < OBJEKTUMLISTA.length; index++) {
-    text += `<div class="part">
-    <ul class="list-group">`;
+    text += `<ul class="list-group col-md-3">`;
 
     text += `<li class="list-group-item"><h2>Neve</h2></li>
     <li class="list-group-item"><p>Fajta, kora</p></li>
     <li class="list-group-item"><button>Gomb</button></li>`;
 
-    text += "</ul><div>";
+    text += "</ul>";
   }
   text += "</div>";
   console.log(text);
