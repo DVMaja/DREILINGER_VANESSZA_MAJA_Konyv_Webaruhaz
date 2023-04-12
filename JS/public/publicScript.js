80e0589f0170f9eab522e804c4d2841677aa25ec
DVMaja <majadreilinger@gmail.com>
2023-04-12 21:28:17 +0200

    átszervezés elötti, működik

--- JS/public/publicScript.js
+++ JS/public/publicScript.js
@@ -10,19 +10,28 @@ function kezdoFuggveny(OBJEKTUMLISTA) {
 
 }
 
-function divekOsszerak(OBJEKTUMLISTA) {  
+function divekOsszerak(lista) {  
   let text = `<div class="tarolo row">`;
 
-  for (let index = 0; index < OBJEKTUMLISTA.length; index++) {
+  for (let index = 0; index < lista.length; index++) {
     text += `<ul class="list-group col-md-3">`;
-
+    const object = lista[index];
+    for (const key in object) {
+      const element = object[key];
+      //<span id="S${key}"> ${element} </span>
+      
+      
+    }
     text += `<li class="list-group-item"><h2>Neve</h2></li>
     <li class="list-group-item"><p>Fajta, kora</p></li>
     <li class="list-group-item"><button>Gomb</button></li>`;
+    
+
+    
 
     text += "</ul>";
   }
   text += "</div>";
-  console.log(text);
+  //console.log(text);
   return text;
 }
