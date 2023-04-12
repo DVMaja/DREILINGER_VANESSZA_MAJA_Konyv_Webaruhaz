60872ffdc2dc92907a0930fe85a99776ad6b6b07
DVMaja <majadreilinger@gmail.com>
2023-04-12 20:12:59 +0200

    alap formázatlan publikus felület proba adatok

--- JS/public/publicScript.js
+++ JS/public/publicScript.js
@@ -1,11 +1,27 @@
-function publikusFelulet(OBJEKTUMLISTA){
-    const taroloElem = $("article .tarolo");  
-  
-    let text;
-    for (let index = 0; index < OBJEKTUMLISTA.length; index++) {
-      text += "<div> </div>"      
-    } 
-  
-    
-  
+import { OBJEKTUMLISTA, kulcsLista } from "../adatok.js";
+
+$(function () {
+  kezdoFuggveny(OBJEKTUMLISTA);
+});
+
+function kezdoFuggveny(OBJEKTUMLISTA) {
+  $(".tarolo").html(divekOsszerak(OBJEKTUMLISTA));
+
+}
+function divekOsszerak(OBJEKTUMLISTA) {
+  const taroloElem = $("article");
+  let text = `<div class="tarolo row">`;
+
+  for (let index = 0; index < OBJEKTUMLISTA.length; index++) {
+    text += `<div class="part"><ul class="list-group">`;
+
+    text += `<li class="list-group-item"><h2>Neve</h2></li>`;
+    text += `<li class="list-group-item"><p>Fajta, kora</p></li>`;
+    text += `<li class="list-group-item"><p><button>Gomb</button></p></li>`;
+
+    text += "</ul><div>";
   }
+  text += "</div>";
+  console.log(text);
+  return text;
+}
