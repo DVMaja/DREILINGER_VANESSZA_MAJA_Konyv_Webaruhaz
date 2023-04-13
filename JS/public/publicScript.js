1573fdb3739a9f2f28e4ba6f220dfe64e5b6dd1b
DVMaja <majadreilinger@gmail.com>
2023-04-13 14:44:48 +0200

    modal alap, meg nem megfelelő adatok

--- JS/public/publicScript.js
+++ JS/public/publicScript.js
@@ -7,6 +7,14 @@ $(function () {
 function kezdoFuggveny(OBJEKTUMLISTA) {
   const taroloElem = $("article");
   $("article").html(divekOsszerak(OBJEKTUMLISTA));
+
+  let modalButton = $(".modalGomb");
+
+  modalButton.on("click", function () {
+    modalButton;
+    let keresettElem = OBJEKTUMLISTA.indexOf(modalButton);
+    kartyakModalja(OBJEKTUMLISTA);
+  });
 }
 
 function divekOsszerak(lista) {
@@ -18,20 +26,40 @@ function divekOsszerak(lista) {
     text += `<div class="card  col-md-3">`;
     for (const key in object) {
       const element = object[key];
-      //<span id="S${key}"> ${element} </span>
       if (key == "nev") {
         text += `<div class="card-header text-bg-success"><h2><span id="S${key}"> ${element} </span></h2></div>`;
       } else {
         text += `<div class="card-body"><p><span id="S${key}"> ${element} </span></p></div>`;
-    ;
       }
     }
-    text += `<div class="card-footer "><button type="button" class="btn btn-dark">Gomb</button></div>`;
+    text += `<div class="card-footer">
+    <button type="button" class="btn btn-dark modalGomb" data-bs-toggle="modal" data-bs-target="#myModal">Gomb</button></div>`;
 
     text += `</div>`;
   }
   text += "</div>";
   text += "</div>";
-  //console.log(text);
   return text;
+}
+
+function kartyakModalja(lista) {  
+  let index = 0;
+  let modal;
+  if (true) {
+    modal += `<div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+     
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+      </div>`;
+  } 
+  return modal;
 }
