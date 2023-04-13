1661fc798ed8e3c1bf51c77954b3b4eaab94ac67
DVMaja <majadreilinger@gmail.com>
2023-04-13 11:20:39 +0200

    card verzio

--- JS/public/publicScript.js
+++ JS/public/publicScript.js
@@ -11,26 +11,23 @@ function kezdoFuggveny(OBJEKTUMLISTA) {
 }
 
 function divekOsszerak(lista) {  
-  let text = `<div class="tarolo row">`;
+  let text = `<div class="tarolo">`;
+  text += `<div class="card row">`;
 
-  for (let index = 0; index < lista.length; index++) {
-    text += `<ul class="list-group col-md-3">`;
+  for (let index = 0; index < lista.length; index++) {    
     const object = lista[index];
     for (const key in object) {
       const element = object[key];
-      //<span id="S${key}"> ${element} </span>
-      
-      
+      //<span id="S${key}"> ${element} </span>      
     }
-    text += `<li class="list-group-item"><h2>Neve</h2></li>
-    <li class="list-group-item"><p>Fajta, kora</p></li>
-    <li class="list-group-item"><button>Gomb</button></li>`;
-    
-
-    
-
-    text += "</ul>";
+    text += `<div class="row">`;
+    text += `<div class="card-header col-md-3"><h2>Neve</h2></div>
+    <div class="card-body col-md-3"><p>Fajta: </p></div>
+    <div class="card-body col-md-3">Kora: </div>
+    <div class="card-footer col-md-3"><button>Gomb</button></div>`;   
+    text += `</div>`
   }
+  text += "</div>";
   text += "</div>";
   //console.log(text);
   return text;
