727dc1e990af7d2dab44c4c3d3fda58edd4f3da5
DVMaja <majadreilinger@gmail.com>
2023-04-13 13:35:29 +0200

    publikus oldal rendezett

--- JS/public/publicScript.js
+++ JS/public/publicScript.js
@@ -1,4 +1,4 @@
-import { OBJEKTUMLISTA} from "../adatok.js";
+import { OBJEKTUMLISTA } from "../adatok.js";
 
 $(function () {
   kezdoFuggveny(OBJEKTUMLISTA);
@@ -7,25 +7,28 @@ $(function () {
 function kezdoFuggveny(OBJEKTUMLISTA) {
   const taroloElem = $("article");
   $("article").html(divekOsszerak(OBJEKTUMLISTA));
-
 }
 
-function divekOsszerak(lista) {  
+function divekOsszerak(lista) {
   let text = `<div class="tarolo">`;
-  text += `<div class="card row">`;
+  text += `<div class="row">`;
 
-  for (let index = 0; index < lista.length; index++) {    
+  for (let index = 0; index < lista.length; index++) {
     const object = lista[index];
+    text += `<div class="card  col-md-3">`;
     for (const key in object) {
       const element = object[key];
-      //<span id="S${key}"> ${element} </span>      
+      //<span id="S${key}"> ${element} </span>
+      if (key == "nev") {
+        text += `<div class="card-header text-bg-success"><h2><span id="S${key}"> ${element} </span></h2></div>`;
+      } else {
+        text += `<div class="card-body"><p><span id="S${key}"> ${element} </span></p></div>`;
+    ;
+      }
     }
-    text += `<div class="row">`;
-    text += `<div class="card-header col-md-3"><h2>Neve</h2></div>
-    <div class="card-body col-md-3"><p>Fajta: </p></div>
-    <div class="card-body col-md-3">Kora: </div>
-    <div class="card-footer col-md-3"><button>Gomb</button></div>`;   
-    text += `</div>`
+    text += `<div class="card-footer "><button type="button" class="btn btn-dark">Gomb</button></div>`;
+
+    text += `</div>`;
   }
   text += "</div>";
   text += "</div>";
